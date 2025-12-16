3aa7f40bde54407dd3c54ee0d2b37874b50ac744
Darragh Boylan <darraghdboylan@gmail.com>
2025-12-16 11:34:50 +0100

    ajout de la page marketing

--- flask/app/controllers/MarketingController.py
+++ flask/app/controllers/MarketingController.py
@@ -1,19 +1,36 @@
-from flask import render_template
+from flask import render_template, request, redirect, url_for, Flask
 from app import app
 from app.controllers.LoginController import reqrole
 
-# class MarketingController:
+from models import MusiqueDAO
 
-#     @app.route('/marketing', methods=['GET'])
-#     @reqrole("marketing")
-#     def marketing_dashboard():
-#         metadata = {"title": "Espace marketing", "pagename": "marketing"}
-#         return render_template('marketing/index.html', metadata=metadata)
+app = Flask(__name__)
+dao = MusiqueDAO()
 
-#     @app.route('/marketing/music/add', methods=['POST'])
-#     @reqrole("marketing")
-#     def add_music():
+@app.route("/")
+def index():
+    sort = request.args.get("sort", "date")
+    musiques = dao.get_5_musiques(sort)
+    return render_template("index.html", musiques=musiques, sort=sort)
 
-#     @app.route('/marketing/music/delete', methods=['POST'])
-#     @reqrole("marketing")
-#     def delete_music():
+@app.route("/delete/<int:id>")
+def delete(id):
+    dao.delete_musique(id)
+    return redirect(url_for("index"))
+
+@app.route("/search_by_titre")
+def search_by_titre():
+    titre = request.args.get("titre")
+    if titre:
+        musiques = dao.get_by_titre(titre)
+        return render_template("index.html", musiques=musiques)
+    return redirect(url_for("index"))
+
+
+@app.route("/search_by_auteur")
+def search_by_auteur():
+    auteur = request.args.get("auteur")
+    if auteur:
+        musiques = dao.get_by_auteur(auteur)
+        return render_template("index.html", musiques=musiques)
+    return redirect(url_for("index"))
---
+++ flask/app/models/Musique.py
@@ -0,0 +1,7 @@
+class Musique:
+    def __init__(self, id, titre, auteur, genre, date):
+        self.id = id
+        self.titre = titre
+        self.auteur = auteur
+        self.genre = genre
+        self.date = date
