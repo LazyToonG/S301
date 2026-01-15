defa31c6a3052532af6097295db8577e8893ae1a
LazyToonG <arthur.shishkovskiy@gmail.com>
2026-01-15 23:10:36 +0100

    copy bizar sup

--- MusiQuali/app/controllers/IndexController.py
+++ MusiQuali/app/controllers/IndexController.py
@@ -29,4 +29,4 @@ class IndexController:
             langue_choisie = 'fr'
         textes = traductions[langue_choisie]
         metadata = {"title": "Accueil", "pagename": "accueil"}
-        return render_template('index.html', metadata=metadata, t=textes, current_lang=langue_choisie)
+        return render_template('testRasp.html', metadata=metadata, t=textes, current_lang=langue_choisie)
