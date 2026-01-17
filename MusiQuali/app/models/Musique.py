78a8aa4c9a688781181f5182157472f488d246b8
Darragh Boylan <darraghdboylan@gmail.com>
2026-01-17 17:14:52 +0100

    recorrection mineure

--- MusiQuali/app/models/Musique.py
+++ MusiQuali/app/models/Musique.py
@@ -1,5 +1,5 @@
 class Musique:
-    def __init__(self, id, titre, auteur, genre, date, chemin, longueure):
+    def __init__(self, id, titre, chemin, longueure):
         self.id = id
         self.titre = titre
         self.chemin = chemin
