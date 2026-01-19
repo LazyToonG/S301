bfd58299b77c881174bddc4e74f0182f328df55c
Darragh Boylan <darraghdboylan@gmail.com>
2026-01-19 22:46:19 +0100

    scripts ssh

---
+++ MusiQuali/app/scripts_sh/becomeserver.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+SSH_CONFIG="/etc/ssh/sshd_config"
+if ! grep -q "^GatewayPorts yes" $SSH_CONFIG; then
+  echo "Activation de GatewayPorts..."
+  echo "GatewayPorts yes" >> $SSH_CONFIG
+fi
+
+echo "Redémarrage du service SSH..."
+systemctl restart ssh
+
