5ef4a38896a49b7194014817655ae34a76ec75d3
LazyToonG <arthur.shishkovskiy@gmail.com>
2026-01-22 17:42:25 +0100

    jspce que j'ai modif

--- MusiQuali/app/controllers/RaspberryController.py
+++ MusiQuali/app/controllers/RaspberryController.py
@@ -33,24 +33,16 @@ def addRaspberry():
     
     return redirect(url_for("admin_dashboard"))
 
-# @app.route("/admin/delete_rasp", methods=["POST"])
-# @reqrole('admin')
-# def delete_user():
- 
-
-#     raspb_id = request.form.get("raspberry-select")
-
-
-#     rs.supprimeR(raspb_id)
-
-#     flash("delete", "success")
-#     return redirect(url_for("admin_dashboard"))
-
 @app.route("/admin/action_rasp", methods=["POST"])
 @reqrole('admin')
 def action_rasp():
     button=request.form.get("action")
-    rasp_id = int(request.form.get("raspberry-select"))
+    rasp_id = request.form.get("raspberry-select")
+    if rasp_id==None:
+        flash("Pas de Raspberry trouvé", "error")
+        return redirect(url_for("admin_dashboard"))
+    else : 
+        rasp_id = int(rasp_id)
     print("rasp_id :",rasp_id)
 
     if not rasp_id:
@@ -71,57 +63,19 @@ def action_rasp():
     
     return redirect(url_for("admin_dashboard"))
 
-<<<<<<< HEAD
-@app.route("/admin/envoie_ping", methods=["POST"])
-@reqrole('admin')
-def envoie_ping():
-    rasp_ip = request.form.get("raspberry-select")
-    r=rs.selectR(rasp_ip)
-    print(rasp_ip)
-    print(r)
-    result = subprocess.run(["ping", "-c", "4", r], capture_output=True, text=True)
-    if result.returncode == 0:
-        flash("Ping et initialisation OK", "success" + rasp_ip)
-    else:
-        flash("Erreur lors de l'initialisation", "error")
-
-    return redirect(url_for("admin_dashboard"))
-=======
-
->>>>>>> 1cef825f27c7b29699a25edbe9a413f1ef4c279c
-
 @app.route("/admin/envoie_ping", methods=["POST"])#chemin doit être changer pour correspondre au bouton sauvegarder planning
 @reqrole('commercial')
 def envoieChaqueChangementPlanning():
     time.sleep(20)  # Attendre 20 secondes avant d'exécuter la fonction pour s'assurer que le fichier est complètement sauvegardé
     raspberrys = rs.findAll()
     for r in raspberrys:
-        subprocess.run(["rsync", "-avze", app.static_fold + "/data/schedule/",  r.nom + "@" + r.ipRasp + ":/home/" + r.nom + "musiquali/"])
-
-
-# ping_status = {"state": "idle", "message": ""}
-
-# @app.route("/admin/envoie_ping", methods=["POST"])
-# @reqrole('admin')
-# def envoie_ping():
-#     def background():
-#         if rs.verifieShellRasp():
-#             ping_status["state"] = "success"
-#             ping_status["message"] = "Ping et initialisation OK"
-#         else:
-#             ping_status["state"] = "error"
-#             ping_status["message"] = "Erreur lors de l'initialisation"
-
-
-#     Thread(target=background).start()
-#     flash("Ping lancé en arrière-plan", "success")
-#     return redirect(url_for("admin_dashboard"))
+        subprocess.run(["rsync", "-avze", app.static_folder + "/data/schedule/",  r.nom + "@" + r.ipRasp + ":/home/" + r.nom + "musiquali/"])
 
 
 def pingTout():
     toutRasp = rs.montreToutRasp()
     for chaque in toutRasp:
-            subprocess.run(["ping", "-c", "1", "192.168.56.104"])
+            subprocess.run(["ping", "-c", "1", chaque["ipRasp"]])
              
 
 
